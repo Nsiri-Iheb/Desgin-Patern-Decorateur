e984a10c8985c4059413d33c14ec3c1fcdf00d01
iheb nciri <iheb.nciri@esprit.tn>
2023-02-11 14:25:42 +0100

    stephan

--- src/Boisson/test.java
+++ src/Boisson/test.java
@@ -1,15 +1,38 @@
 package Boisson;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.Scanner;
 
 public class test {
-	public static void main(String[] args) {
-		Boisson b=new Espresso();
-		System.out.println(b.getDescription());
-		System.out.println(b.cout());
-		b=new Choclat(b);
-		System.out.println(b.cout());
-		b=new Caramel(b);
-		System.out.println(b.cout());
-		 
-	}
+	public static void main(String[] args)  {
+		
 
-}
+		
+		        Scanner sc = new Scanner(System.in);
+		        int t = sc.nextInt();
+		        String codeforces = "codeforces";
+		        while (t-- > 0) {
+		            char c = sc.next().charAt(0);
+		            if (codeforces.indexOf(c) >= 0) {
+		                System.out.println("YES");
+		            } else {
+		                System.out.println("NO");
+		            }
+		        }
+		        sc.close();
+		    }
+		
+
+	
+	    
+		
+		       
+		     
+		    
+		}
+
+
+
+
